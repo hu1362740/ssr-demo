0ceb7904341b67801d93bad94c956e57dc598e5c
DESKTOP-DDJLKB3\China <hzq2006126@126.com>
2023-01-29 15:14:24 +0800

    feat：构架实现1完成

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,16 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>hello-aaaa</h1>
+      <button
+        onClick={(): void => {
+          alert("hello-aaaa");
+        }}
+      >
+        alert
+      </button>
+    </div>
+  );
+};
+
+export default Home;
