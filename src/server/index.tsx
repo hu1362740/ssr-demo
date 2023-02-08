d4da2e0f25637a07a8ad0c296b861d34513823c2
DESKTOP-DDJLKB3\China <hzq2006126@126.com>
2023-02-08 15:59:33 +0800

    feat： 架构实现2

--- src/server/index.tsx
+++ src/server/index.tsx
@@ -1,20 +1,70 @@
 import express from "express";
 import childProcess from "child_process";
 import { renderToString } from "react-dom/server";
-import Home from "../pages/Home";
+import path from "path";
+import router from "@/router";
+import { Route, Routes } from "react-router-dom";
+import { StaticRouter } from "react-router-dom/server";
+import { Helmet } from "react-helmet";
+import { serverStore } from "@/store";
+import { Provider } from "react-redux";
 
 const app = express();
-const content = renderToString(<Home />)
+
+const bodyParser = require("body-parser");
+
+app.use(express.static(path.resolve(process.cwd(), "client_build")));
+
+// 请求body解析
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// 启一个post服务
+app.post("/api/getDemoData", (req, res) => {
+  res.send({
+    data: req.body,
+    status_code: 0,
+  });
+})
 
 app.get("*", (req, res) => {
+  const content = renderToString(
+    <Provider store={serverStore}>
+      <StaticRouter location={req.path}>
+        <Routes>
+          {router?.map((item, index) => {
+            return <Route {...item} key={index} />;
+          })}
+        </Routes>
+      </StaticRouter>
+    </Provider>
+  );
+
+  const helmet = Helmet.renderStatic();
+
   res.send(`
-    <html
-      <body>
-        <div>${content}</div>
-      </body>
-    </html>
-  `);
+  <html
+    <head>
+      ${helmet.title.toString()}
+      ${helmet.meta.toString()}
+    </head>
+    <body>
+      <div id="root">${content}</div>
+      <script src="/index.js"></script>
+    </body>
+  </html>
+`);
 });
+// app.get("*", (req, res) => {
+//   res.send(`
+//     <html
+//       <body>
+//       <div id="root">${content}</div>
+//       <script src="/index.js"></script>
+//       </body>
+//     </html>
+//   `);
+// });
 
 app.listen(3000, () => {
   console.log("ssr-server listen on 3000");
